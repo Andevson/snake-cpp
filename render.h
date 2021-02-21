b6e9aad7ce3bc7fcd3caabfd412c98a0e3cb690b
Andevson <andersonteodorodejesus@hotmail.com>
2021-02-20 21:42:12 -0300

    update render.h

--- render.h
+++ render.h
@@ -1,11 +1,16 @@
 #include <iostream>
+#include "mat.h"
+#include "cfg.h"
+#include "object.h"
+#include "game.h"
 
 #ifndef RENDER_CPP
 #define RENDER_CPP
 
-#include "mat.h"
-#include "cfg.h"
-#include "object.h"
+using namespace std;
+
+char draw_buffer;
+//Draw with absolute position
 void drawSnake(void){
 	COLOR_SNAKE;
 	std::cout << " O";
@@ -21,7 +26,11 @@ void drawFruit(void){
 }
 void drawBonus(int life_time){
 	COLOR_BONUS;
-	std::cout << " " << life_time;
+	if(life_time < 10){
+		std::cout << " " << life_time;
+	}else{
+		std::cout << life_time;
+	}
 	COLOR_WALL;
 }
 void drawEmpty(void){
@@ -34,7 +43,7 @@ void drawScore(int score){
 	std::cout << score << std::endl;
 	COLOR_WALL;
 }
-char draw_buffer;
+//Draw
 void drawBuffer(char buffer){
 	switch(buffer){
 		case ' ':
@@ -59,19 +68,15 @@ void drawBuffer(char buffer){
 		drawBonus(buffer);
 	}
 }
-void renderGame(game_config* cfg, objects* objs) {
-	if(objs->getSnake() == NULL){
-		throwError("Error_rendering_empty_snake");
-		return;
-	}
+void renderGame(game* g, game_config* cfg, objects* objs) {
 	system("cls");
-	drawScore(cfg->score);
+	drawScore(g->getScore());
 	_2D scan = { 0, 0 };
-	for (scan.y = 0; scan.y <= cfg->stage.y + 1; scan.y += 1) {
-		for (scan.x = 0; scan.x <= cfg->stage.x + 1; scan.x ++) {
+	for (scan.y = 0; scan.y <= cfg->getStage().y + 1; scan.y += 1) {
+		for (scan.x = 0; scan.x <= cfg->getStage().x + 1; scan.x ++) {
 			draw_buffer = ' ';
 			snake* target_snake = objs->getSnake();
-			if (scan.x == 0 || scan.x == cfg->stage.x + 1 || scan.y == 0 || scan.y == cfg->stage.y + 1) {
+			if (scan.x == 0 || scan.x == cfg->getStage().x + 1 || scan.y == 0 || scan.y == cfg->getStage().y + 1) {
 				draw_buffer = 'w';
 			}
 			while(target_snake != NULL){
@@ -80,14 +85,16 @@ void renderGame(game_config* cfg, objects* objs) {
 				}
 				target_snake = target_snake->getNext();
 			}
-			for (int i = 1; i <= objs->quantity; i++) {
-				if (equal(scan, objs->getFruit(i).getPosition())) {
-					if(objs->getFruit(i).getType() == 'f'){
-						draw_buffer = 'f';
-					}else if(objs->getFruit(i).getType() == 'b'){
-						draw_buffer = objs->getFruit(i).getLifeTime();
-					}else{
-						draw_buffer = 'e';
+			if(objs->getLength() > 0){
+				for (int i = 1; i <= objs->getLength(); i++) {
+					if (equal(scan, objs->getFruit(i).getPosition())) {
+						if(objs->getFruit(i).getType() == 'f'){
+							draw_buffer = 'f';
+						}else if(objs->getFruit(i).getType() == 'b'){
+							draw_buffer = objs->getFruit(i).getLifeTime();
+						}else{
+							draw_buffer = 'e';
+						}
 					}
 				}
 			}
@@ -96,13 +103,13 @@ void renderGame(game_config* cfg, objects* objs) {
 		std::cout << endl;
 	}
 }
-void renderMenu(game_config* cfg) {
+void renderMenu(game* g, game_config* cfg) {
 	system("cls");
 	COLOR_TEXT;
-	_2D menu_position = { (int)cfg->stage.x/2 - 4, (int)cfg->stage.y/2 - 4};
+	_2D menu_position = { (int)cfg->getStage().x/2 - 4, (int)cfg->getStage().y/2 - 4};
 	_2D scan = { 0, 0 };
-	for (scan.y = 0; scan.y <= cfg->stage.y + 1; scan.y += 1) {
-		for (scan.x = 0; scan.x <= cfg->stage.x + 1; scan.x += 1) {
+	for (scan.y = 0; scan.y <= cfg->getStage().y + 1; scan.y += 1) {
+		for (scan.x = 0; scan.x <= cfg->getStage().x + 1; scan.x += 1) {
 			if (scan.y == menu_position.y) {
 				if (scan.x == menu_position.x + 1) {
 					COLOR_WARNING;
@@ -118,17 +125,17 @@ void renderMenu(game_config* cfg) {
 				if (scan.x == menu_position.x) {
 					std::cout << " Your Score   : ";
 					COLOR_SCORE;
-					if (cfg->score <= 9) {
+					if (g->getScore() <= 9) {
 						std::cout << " 0";
 						scan.x += 1;
-					}else if (cfg->score <= 99) {
-						std::cout << cfg->score;
+					}else if (g->getScore() <= 99) {
+						std::cout << g->getScore();
 						scan.x += 1;
-					}else if (cfg->score <= 999) {
-						std::cout << " " << cfg->score;
+					}else if (g->getScore() <= 999) {
+						std::cout << " " << g->getScore();
 						scan.x += 2;
-					}else if (cfg->score <= 9999) {
-						std::cout << cfg->score;
+					}else if (g->getScore() <= 9999) {
+						std::cout << g->getScore();
 						scan.x += 2;
 					}
 					COLOR_TEXT;
@@ -149,15 +156,15 @@ void renderMenu(game_config* cfg) {
 			}
 			else if (scan.y == menu_position.y + 5) {
 				if (scan.x == menu_position.x + 2) {
-					if (cfg->game_status == 'c') {
+					if (g->getStatus() == "menu_play") {
 						COLOR_SELECTION;
-						std::cout << "-> Continue ";
+						std::cout << "-> Play ";
 						COLOR_TEXT;
-						scan.x += 5;
+						scan.x += 3;
 					}
 					else {
-						std::cout << "Continue  ";
-						scan.x += 4;
+						std::cout << "Play. ";
+						scan.x += 2;
 					}
 				}
 				else {
@@ -166,7 +173,7 @@ void renderMenu(game_config* cfg) {
 			}
 			else if (scan.y == menu_position.y + 6) {
 				if (scan.x == menu_position.x + 2) {
-					if (cfg->game_status == 'q') {
+					if (g->getStatus() == "menu_quit") {
 						COLOR_SELECTION;
 						std::cout << "-> Quit.";
 						COLOR_TEXT;
@@ -245,6 +252,13 @@ void renderMenu(game_config* cfg) {
 			}
 		}
 		std::cout << std::endl;
+	}
+}
+void render(game* g, game_config* cfg, objects* objs){
+	if(g->getStatus() == "game_playing"){
+		renderGame(g, cfg, objs);
+	}else{
+		renderMenu(g, cfg);
 	}
 }
 
